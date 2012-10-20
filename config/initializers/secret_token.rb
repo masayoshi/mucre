2f863f77508a133b3d464d3217af5c51d0154aa1
masay0shi <masayoshi.cdm@gmail.com>
2012-10-20 10:29:36 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mucre::Application.config.secret_token = '7c3fbede0622f928d60eb5721da249217d44a64277c41cb0b5dd3f041f58abed0d6ba00fe80bdccef7329eb89fcfc94563a6a96a6037f42824bae4184de219c2'

4c87371dd7216d06c3916da6b507834f98064d8a
masayoshi <masayoshi.cdm@gmail.com>
2013-03-02 16:49:00 -0800

    Change Site Name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   include ActsAsTaggableOn::TagsHelper
   # Returns the full title on a per-page basis.
   def full_title page_title
-    base_title = "MuseumCreated.com"
+    base_title = "SPOT"
     if page_title.empty?
       base_title
     else

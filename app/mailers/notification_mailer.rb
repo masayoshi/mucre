b7a8980b4941230fb20882c8041639b3ab068527
masayoshi <masayoshi.cdm@gmail.com>
2013-04-12 05:52:42 -0700

    Fix send email bug from heroku

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,13 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "info@mu-cre.com"
-  default to: "mu-cre@googlegroups.com"
+  default from: "info@mu-cre.com", to: "info@mu-cre.com"
 
   def event_registration_notification(event)
     @event = event
     mail( subject: "Event Registration Notification")
+  end
+
+  def test_event_registration_notification(event, email)
+    @event = event
+    mail( to: email, subject: "Event Registration Notification")
   end
 end
